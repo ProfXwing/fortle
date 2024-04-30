44cb873a3f2ea5c4d766d5edbea92126c09b685f
ProfXwing <50530928+ProfXwing@users.noreply.github.com>
2024-04-30 00:42:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fortle/",
+  base: "/wordle/",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
